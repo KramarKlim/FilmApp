f49dd8baa1489a30ba9f26c4e91903914922f01e
KramarKlim <kramarklim@yandex.ru>
2021-08-31 18:30:21 +0200

    Initial Commit

---
+++ FilmApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FilmApp
+//
+//  Created by Клим on 31.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
