22d23e26dd8f8624903d465354a2abbf8113ff8f
KramarKlim <kramarklim@yandex.ru>
2021-09-07 08:42:44 +0200

    Data Manager private properties

--- FilmApp/Managers/DataManager.swift
+++ FilmApp/Managers/DataManager.swift
@@ -14,26 +14,36 @@ class DataManager {
     
     var favourite = Set<Int>()
     
+    private let webString = "https://api.themoviedb.org/3/"
+    
+    private let keyString = "?api_key=b20dda5a33582cd5efa3a442b91f10ec"
+    
     private init() {}
     
     func getShowList(show: CurrentShow) -> String {
         switch show {
-        case .film: return "https://api.themoviedb.org/3/discover/movie?api_key=b20dda5a33582cd5efa3a442b91f10ec"
-        case .serial: return "https://api.themoviedb.org/3/discover/tv?api_key=b20dda5a33582cd5efa3a442b91f10ec"
+        case .film:
+            return webString + "discover/movie" + keyString
+        case .serial:
+            return webString + "discover/tv" + keyString
         }
     }
     
     func getDetail(detail: CurrentShow, id: Int) -> String {
         switch detail {
-        case .film: return "https://api.themoviedb.org/3/movie/\(id)?api_key=b20dda5a33582cd5efa3a442b91f10ec"
-        case .serial: return "https://api.themoviedb.org/3/tv/\(id)?api_key=b20dda5a33582cd5efa3a442b91f10ec"
+        case .film:
+            return webString + "movie/\(id)" + keyString
+        case .serial:
+            return webString + "tv/\(id)" + keyString
         }
     }
     
     func getActor(detail: CurrentShow,id: Int) -> String {
         switch detail {
-        case .film: return "https://api.themoviedb.org/3/movie/\(id)/credits?api_key=b20dda5a33582cd5efa3a442b91f10ec"
-        case .serial: return "https://api.themoviedb.org/3/tv/\(id)/credits?api_key=b20dda5a33582cd5efa3a442b91f10ec"
+        case .film:
+            return webString + "movie/\(id)/credits" + keyString
+        case .serial:
+            return webString + "tv/\(id)/credits" + keyString
         }
     }
     
@@ -43,9 +53,10 @@ class DataManager {
     
     func getError(error: Error) -> String {
         switch error {
-        case .image: return "https://w7.pngwing.com/pngs/340/956/png-transparent-profile-user-icon-computer-icons-user-profile-head-ico-miscellaneous-black-desktop-wallpaper.png"
-        case .web: return "https://www.themoviedb.org"
+        case .image:
+            return "https://w7.pngwing.com/pngs/340/956/png-transparent-profile-user-icon-computer-icons-user-profile-head-ico-miscellaneous-black-desktop-wallpaper.png"
+        case .web:
+            return "https://www.themoviedb.org"
         }
     }
 }
-
